edda7eca20cf7702d27f445378dd5044962538a3
NavayuvanSB <sb.navayuvan@gmail.com>
2021-03-29 11:35:34 +0530

    Add stub view functions

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,17 @@
 from django.http import HttpResponse
 
 
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+
+def results(request, question_id):
+    return HttpResponse("You're looking at results of question %s." % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
+
+
 def index(request):
     return HttpResponse("Hey buddy! Welcome to django!")
