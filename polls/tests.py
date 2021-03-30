2e110a1e164ce551b78008c4a85029d33b7f2ec8
navayuvansb <sb.navayuvan@gmail.com>
2021-03-30 12:36:43 +0530

    Add test cases for Details View

--- polls/tests.py
+++ polls/tests.py
@@ -91,6 +91,36 @@ class QuestionIndexViewTests(TestCase):
         )
 
 
+class QuestionDetailViewTests(TestCase):
+
+    def test_future_question(self):
+        """
+        The detail view of question with the pub_date in the future
+        should return 404 not found.
+        """
+
+        future_question = create_question(
+            "Future question.", days_offset_from_now=30)
+        url = reverse("polls:detail", args=(future_question.id,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
+
+    def test_past_question(self):
+        """
+        The detail view of question with the pub_date in the past should
+        return the question text and options
+        """
+
+        past_question = create_question(
+            "Past question.", days_offset_from_now=-1
+        )
+        url = reverse("polls:detail", args=(past_question.id,))
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, past_question.question_text)
+
+
 class QuestionModelTests(TestCase):
 
     def test_was_published_recently_with_future_question(self):
